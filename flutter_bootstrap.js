312ac761ade7a022f563c9aff06af2821062b740
Артем Мовчан <mob.ar@mail.ru>
2024-11-14 14:51:42 +0400

    build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1833138370"
+    serviceWorkerVersion: "2844291393"
   }
 });
